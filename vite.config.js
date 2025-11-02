5e7b1fb60ef3fbd5d5e86b1f3ee569eddb957aad
Alan Evagelista <138069995+AlanPEvangelista@users.noreply.github.com>
2025-11-01 21:21:50 -0300

    vite ddns

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    hmr: {
+      host: 'casaalan.ddns.net',
+    },
+  },
+  preview: {
+    host: true,
+    port: 8082,
+  },
 })
